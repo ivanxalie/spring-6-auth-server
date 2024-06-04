d1c167e63b960320fbdbfeb4abc5c4c365b749b2
ivanxalie <ivanxalie@gmail.com>
2024-06-04 21:19:17 +0300

    Add actuator

--- src/main/java/guru/springframework/spring6authserver/config/SecurityConfig.java
+++ src/main/java/guru/springframework/spring6authserver/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -140,5 +141,14 @@ public class SecurityConfig {
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity security) throws Exception {
+        return security
+                .authorizeHttpRequests(matcherRegistry -> matcherRegistry.requestMatchers(EndpointRequest.toAnyEndpoint())
+                        .permitAll())
+                .build();
     }
 }
